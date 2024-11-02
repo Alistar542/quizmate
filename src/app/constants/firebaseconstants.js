e4f5cfb6cba1cf43d6ec0c443d04a7a40e7862c5
Alistar542 <alistarfz@gmail.com>
2024-11-02 20:55:17 +0100

    change to firebase constants

--- src/app/constants/firebaseconstants.js
+++ src/app/constants/firebaseconstants.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: process.env.MEASUREMENT_ID,
-    authDomain: process.env.APP_ID,
-    projectId: process.env.MESSAGING_SENDER_ID,
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.PROJECT_ID,
-    appId: process.env.FIREBASE_AUTH_DOMAIN,
-    measurementId: process.env.FIREBASE_API_KEY
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
   };
