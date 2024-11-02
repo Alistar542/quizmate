39ac927e90bc7a8236f9563d65a183f955947be3
Alistar542 <alistarfz@gmail.com>
2024-11-02 20:37:37 +0100

    firebase constants

--- src/app/constants/firebaseconstants.js
+++ src/app/constants/firebaseconstants.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: process.env.MEASUREMENT_ID,
+    authDomain: process.env.APP_ID,
+    projectId: process.env.MESSAGING_SENDER_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.PROJECT_ID,
+    appId: process.env.FIREBASE_AUTH_DOMAIN,
+    measurementId: process.env.FIREBASE_API_KEY
   };
